ce18a640602adc7bf64ed732b410b932018c6407
Igntion <emari.wright@avanade.com>
2024-08-06 11:23:34 -0500

    Update v2vm1script.ps1

--- v2vm1script.ps1
+++ v2vm1script.ps1
@@ -62,30 +62,3 @@ while ((Get-WindowsFeature -Name AD-Domain-Services).InstallState -ne "Installed
     Start-Sleep -Seconds 10
 }
 
-# Import the ADDSDeployment module
-Import-Module ADDSDeployment
-
-# Install AD DS Forest
-Install-ADDSForest `
-  -CreateDnsDelegation:$false `
-  -DatabasePath "C:\Windows\NTDS" `
-  -DomainMode "WinThreshold" `
-  -DomainName $domainName `
-  -DomainNetbiosName "CORE" `
-  -ForestMode "WinThreshold" `
-  -InstallDns:$true `
-  -LogPath "C:\Windows\NTDS" `
-  -NoRebootOnCompletion:$false `
-  -SysvolPath "C:\Windows\SYSVOL" `
-  -SafeModeAdministratorPassword $dsrmPassword `
-  -Force:$true
-
-# Create a scheduled task to run the post-restart script after restart
-$action = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument "-File $postRestartScriptPath"
-$trigger = New-ScheduledTaskTrigger -AtStartup
-$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
-$task = New-ScheduledTask -Action $action -Trigger $trigger -Principal $principal
-Register-ScheduledTask -TaskName "PostRestartScript" -InputObject $task
-
-# Restart the server
-Restart-Computer -Force
